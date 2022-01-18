d01cd22c8babaa86805d145e4f69332fe11c708e
RevelDeng <84701226+RevelDeng@users.noreply.github.com>
2022-01-18 04:31:40 -0500

    my first React code

---
+++ my-react-app/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const myfirstelement = <h1>My First React Code!</h1>
+
+ReactDOM.render(
+  myfirstelement,
+  document.getElementById('root')
+);
