ec422a1e943cf018e7cdc1c2b6d7d48977830723
RevelDeng <84701226+RevelDeng@users.noreply.github.com>
2022-01-19 03:15:51 -0500

    added more React code

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const myfirstelement = <h1>My First React Code!</h1>
+var x = 5
+
+const myfirstelement = (
+  <>
+    <h1>My First React Code!</h1>
+    <ul>
+      <li>{5 + 5}</li>
+      <li>{6 * 6}</li>
+    </ul>
+    <p>{x < 10 ? x : x + 5}</p>
+  </>
+)
 
 ReactDOM.render(
   myfirstelement,
