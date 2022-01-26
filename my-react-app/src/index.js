1cde889c8c7f250b00d7059b82676eba2ee96261
RevelDeng <84701226+RevelDeng@users.noreply.github.com>
2022-01-26 01:32:31 -0500

    added components and event handler

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -10,11 +10,36 @@ const myfirstelement = (
       <li>{5 + 5}</li>
       <li>{6 * 6}</li>
     </ul>
-    <p>{x < 10 ? x : x + 5}</p>
+    <p>{x < 10 ? x : x + 5}</p> 
   </>
 )
+
+function Car(props) {
+  return <h2>This is my {props.brand.color} {props.brand.name} {props.brand.model}!</h2>
+}
+
+function Garage() {
+  const carInfo = {
+    name: "Pontiac",
+    model: "Grand Am",
+    color: "teal"
+  }
+  const start = () => {
+    alert("Starting up the car")
+  }
+  return <>
+    <h3>These cars are in my garage:</h3>
+    <Car brand={carInfo} />
+    <button onClick={start}>Start the car</button>
+  </>
+}
 
 ReactDOM.render(
   myfirstelement,
   document.getElementById('root')
 );
+
+ReactDOM.render(
+  <Garage/>,
+  document.getElementById('second_root')
+)
