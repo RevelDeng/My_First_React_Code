07b23477cf2bb7ea6dc701685fba97ec949d3049
RevelDeng <84701226+RevelDeng@users.noreply.github.com>
2022-01-27 21:59:10 -0500

    added conditional and form

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
 var x = 5
@@ -15,23 +15,45 @@ const myfirstelement = (
 )
 
 function Car(props) {
-  return <h2>This is my {props.brand.color} {props.brand.name} {props.brand.model}!</h2>
+  return <h2>This is my {props.color} {props.name} {props.model}!</h2>
 }
 
 function Garage() {
-  const carInfo = {
-    name: "Pontiac",
-    model: "Grand Am",
-    color: "teal"
-  }
-  const start = () => {
-    alert("Starting up the car")
+  const cars = [
+    {id: 1, name: "Pontiac", model: "Grand Am", color: "teal"},
+    {id: 2, name: "Ford", model: "Fiesta", color: "mahogany"},
+    {id: 3, name: "Subaru", model: "Crosstrek", color: "gold"}
+  ]
+  const start = (startchance) => {
+    if (startchance < 0.5) {
+      alert("Starting up the car")
+    }
+    else {
+      alert("Failed to start up the car")
+    }
   }
   return <>
     <h3>These cars are in my garage:</h3>
-    <Car brand={carInfo} />
-    <button onClick={start}>Start the car</button>
+    <ul>
+      {cars.map((car) => <Car key={car.id} name={car.name} model={car.model} color={car.color} />)}
+    </ul>
+    <button onClick={() => start(Math.random())}>Start the car</button>
   </>
+}
+
+function Form() {
+  const [name, setName] = useState("")
+
+  return (
+    <form>
+      <label>Enter your name:
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+    </form>
+  )
 }
 
 ReactDOM.render(
@@ -42,4 +64,9 @@ ReactDOM.render(
 ReactDOM.render(
   <Garage/>,
   document.getElementById('second_root')
+)
+
+ReactDOM.render(
+  <Form/>,
+  document.getElementById('third_root')
 )
