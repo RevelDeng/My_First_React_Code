38ca7da145be7f9ef4f5c80614d43d2cf5a01b4e
RevelDeng <84701226+RevelDeng@users.noreply.github.com>
2022-01-31 00:47:38 -0500

    added form submit button and alert

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -44,14 +44,20 @@ function Garage() {
 function Form() {
   const [name, setName] = useState("")
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    alert("Your name: ${name}")
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>Enter your name:
         <input
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
       </label>
+      <input type="submit"/>
     </form>
   )
 }
