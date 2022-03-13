369d280681fe257b9fd6551b295346b594dc6c23
RevelDeng <84701226+RevelDeng@users.noreply.github.com>
2022-03-13 00:01:10 -0500

    added inline styling to contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,7 @@
 import {memo} from "react";
 
 const Contact = () => {
-    return <h1>Contact Me</h1>;
+    return <h1 style={{fontStyle: "italic"}}>Contact Me</h1>;
 }
 
 export default memo(Contact);
